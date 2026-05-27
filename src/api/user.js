060f204e1787ada2a90cc660f386659061670fa1
t13CCC <1346302297@qq.com>
2026-05-27 21:48:51 +0800

    06

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from "@/utils/request";
 
 export function loginByEmail(email, password) {
   return request({
-    url: "http://127.0.0.1:4523/m1/8273217-8035745-7748640/api/auth/login/email",
+    url: "/api/auth/login/email",
     method: "POST",
     data: {
       email,
@@ -43,10 +43,13 @@ export function sendVerifyCode(email) {
   });
 }
 
-export function getUserInfo() {
+export function getUserInfo(userId) {
   return request({
     url: "/api/user",
     method: "GET",
+    params: {
+      userId,
+    },
   });
 }
 
